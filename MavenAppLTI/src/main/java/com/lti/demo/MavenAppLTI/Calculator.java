c97542cea0251f3e1221a735970aa1af5b40035c
shreya10 <galurgi@gmail.com>
2021-07-29 15:56:23 +0530

    my commit

---
+++ MavenAppLTI/src/main/java/com/lti/demo/MavenAppLTI/Calculator.java
@@ -0,0 +1,10 @@
+package com.lti.demo.MavenAppLTI;
+
+public class Calculator {
+     public int add(int x,int y) {
+    	 return x+y;
+     }
+     public int prod(int x,int y) {
+    	 return x*y;
+     }
+}
---
+++ MavenAppLTI/src/test/java/com/lti/demo/MavenAppLTI/CalculatorTest.java
@@ -0,0 +1,39 @@
+package com.lti.demo.MavenAppLTI;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd1() {
+		int res=new Calculator().add(10, 30);
+		assertEquals(30,res);
+		
+	}
+	@Test
+	public void testAdd2() {
+		int res=new Calculator().add(-10, 20);
+		assertEquals(10,res);
+		
+	}
+	@Test
+	public void prod() {
+		int res=new Calculator().prod(1, 3);
+		assertEquals(3,res);
+		
+	}
+	@Test
+	public void prod1() {
+		int res=new Calculator().add(10, 30);
+		assertEquals(300,res);
+		
+	}
+
+}
